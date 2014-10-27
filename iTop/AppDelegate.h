f2bc40915edf64e5c54d9432d6792a0254c017e7
Brad Slayter <BradleySlayter@my.unt.edu>
2014-10-27 09:23:08 -0500

    Initial Commit

---
+++ iTop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTop
+//
+//  Created by Bradley Slayter on 10/27/14.
+//  Copyright (c) 2014 Flipped Bit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iTop
+//
+//  Created by Bradley Slayter on 10/27/14.
+//  Copyright (c) 2014 Flipped Bit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
